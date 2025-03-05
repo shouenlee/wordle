1b9e0e463981966a0513aaa6a9bafb477bcdc3ba
Shouen Lee (from Dev Box) <shouenlee@microsoft.com>
2025-03-04 20:51:20 -0800

    add prod vs dev envs

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',  // Enable static exports
-  basePath: '/wordle', // Replace with your repository name
+  output: process.env.NODE_ENV === 'production' ? 'export' : undefined,
+  basePath: process.env.NODE_ENV === 'production' ? '/wordle' : '',
   images: {
     unoptimized: true,
   },
